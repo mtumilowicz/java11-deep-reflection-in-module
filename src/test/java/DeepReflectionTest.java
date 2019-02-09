3438f7dc26d5c8612a4c21e3cba344211a5489e1
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-02-09 21:36:00 +0100

    junit 5 + gradle java modularity config

--- src/test/java/DeepReflectionTest.java
+++ src/test/java/DeepReflectionTest.java
@@ -1,26 +1,26 @@
 import company.Accessor;
 import company.Company;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 /**
  * Created by mtumilowicz on 2019-02-09.
  */
-public class DeepReflectionTest {
-    
+class DeepReflectionTest {
+
     @Test
-    public void getPrivateField() throws Exception {
-        assertThat(Accessor.get(), is("privateField"));
+    void getPrivateField() throws Exception {
+        assertEquals("privateField", Accessor.get());
     }
 
     @Test
-    public void setPrivateField() throws Exception {
+    void setPrivateField() throws Exception {
         var company = new Company();
-        
+
         Accessor.set("new field value", company);
-        
-        assertThat(company.getPrivateField(), is("new field value"));
+
+        assertEquals("new field value", company.getPrivateField());
     }
 }
